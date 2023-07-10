4529ad46733ca1f33fe71d0889d026733c697043
batuhansariikaya <batuhansarikaya008@gmail.com>
2023-07-10 13:52:08 +0300

    repository interfaceleri yazıldı

---
+++ ResumeProject.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,21 @@
+﻿using ResumeProject.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeProject.Application.Repositories
+{
+    public interface IWriteRepository<T>:IRepository<T> where T : BaseEntity
+    {
+        void Add(T entity);
+        Task<bool> AddAsync(T entity);
+        Task<bool> AddRangeAsync(List<T> entity);
+        bool Remove(T entity);
+        Task<bool> RemoveAsync(int id);
+        bool Update(T model);
+        Task<int> SaveAsync();
+
+    }
+}
